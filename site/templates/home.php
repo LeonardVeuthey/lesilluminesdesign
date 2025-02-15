d3d42c5c00edc7e6caca6f5e4c759e491f213480
Komball1213 <veuthey.leonard@gmail.com>
2025-02-15 11:25:00 +0100

    mod_soon-2

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,7 +4,7 @@
 
 <div class="soon">
   <h1 class="soon_text">Les Illuminés Design</h1>
-  <p class="soon_text">Site en construction</p>
+  <p class="soon_text">Un nouveau site arrive bientôt</p>
 </div>
 
 
