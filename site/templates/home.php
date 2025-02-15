6af5bfa898eb26da77a437730cded741135f9533
Komball1213 <veuthey.leonard@gmail.com>
2025-02-15 11:47:30 +0100

    mod_errorpage

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,6 +5,7 @@
 <div class="soon">
   <h1 class="soon_text">Les Illuminés Design</h1>
   <p class="soon_text">Un nouveau site arrive bientôt</p>
+  <p class="soon_text">info@lesilluminesdesign.ch</p>
 </div>
 
 
