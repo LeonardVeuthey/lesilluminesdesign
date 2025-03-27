838a1d25cd94cbb3bd024c68f233698de5c267bc
Komball1213 <veuthey.leonard@gmail.com>
2025-03-27 11:12:00 +0100

    image

--- site/snippets/galerie.php
+++ site/snippets/galerie.php
@@ -2,7 +2,7 @@
         <div class="galerie">
           <?php foreach ($page->galerie()->toFiles() as $image): ?>
             <figure class="galerie_item">
-              <img src="<?= $image->url() ?>" alt="<?= $image->alt()->or($image->filename()) ?>">
+              <img class="galerie_image" src="<?= $image->url() ?>" alt="<?= $image->alt()->or($image->filename()) ?>">
               <?php if ($image->caption()->isNotEmpty()): ?>
                 <figcaption><?= $image->caption()->html() ?></figcaption>
               <?php endif; ?>
